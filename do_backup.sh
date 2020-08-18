1e952e303162ede85b77130fbf1d76e02d4db298
pr498jg <ralf.jungmann@bundesbank.de>
2020-08-18 07:50:39 +0000

    alle Aktionen in einen Loop eingefügt

--- do_backup.sh
+++ do_backup.sh
@@ -3,33 +3,27 @@
 # Dieses Script legt 9 Generationen des htdocs-Verzeichnisses unter Nutzung von Hardlinks an
 #
 declare -a BACKUPDIR
-ROOTDIR=/opt/bbk/was
-#ROOTDIR=~
+#ROOTDIR=/opt/bbk/was
+GENERATIONS=9
+ROOTDIR=~
 DIRECTORY=pkg
 
 SOURCEDIR=${ROOTDIR}/${DIRECTORY}
 
-for I in 0 1 2 3 4 5 6 7 8 9
+for I in $(seq $GENERATIONS -1 0)
 do
   BACKUPDIR[I]=${ROOTDIR}/.${DIRECTORY}.${I}
   #echo ${BACKUPDIR[$I]}
-done
-
-# letzte Generation löschen
-[ -d ${BACKUPDIR[9]} ] && rm -rf ${BACKUPDIR[9]}
-
-# Generationen 1-8 um eins verschieben
-for I in 8 7 6 5 4 3 2 1
-do
+  # letzte Generation löschen
+  [ -d ${BACKUPDIR[$GENERATIONS]} ] && ( rm -rf ${BACKUPDIR[$GENERATIONS]} ; continue) 
+  # Generationen 1 - ($GENERATIONS - 1) verschieben
   J=$(($I + 1))
   [ -d ${BACKUPDIR[$I]} ] && mv ${BACKUPDIR[$I]} ${BACKUPDIR[$J]}
+  # Generation 0 per Hardlink nach Gen 1 kopieren
+  [ -d ${BACKUPDIR[0]} ] && cp -al ${BACKUPDIR[0]} ${BACKUPDIR[1]}
+  # Source nach Gen 0 kopieren
+  rsync -q -av -H --delete $SOURCEDIR ${BACKUPDIR[0]}
+  # Backupdir 0 mit Zeitstempel versehen
+  touch ${BACKUPDIR[0]}
 done
 
-# Generation 0 per Hardlink nach Gen 1 kopieren
-[ -d ${BACKUPDIR[0]} ] && cp -al ${BACKUPDIR[0]} ${BACKUPDIR[1]}
-
-# Source nach Gen 0 kopieren
-rsync -q -av -H --delete $SOURCEDIR ${BACKUPDIR[0]}
-
-# Backupdir 0 mit Zeitstempel versehen
-touch ${BACKUPDIR[0]}
